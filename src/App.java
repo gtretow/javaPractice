57dbee782a53b2d0a358151dc423b63df0bc3dbc
gtretow <guilherme.tretow@gmail.com>
2024-06-10 16:40:45 -0300

    added trycatch

--- src/App.java
+++ src/App.java
@@ -1,20 +1,32 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        double bankAccount = 400.00;
+
         try (Scanner scanner = new Scanner(System.in)) {
             System.out.println("Digite seu nome:");
             String userName = scanner.nextLine();  
 
             System.out.println("Digite o valor a depositar:");
-            String depositAmount = scanner.nextLine(); 
+            double depositAmount = scanner.nextDouble(); 
 
             System.out.println("Digite o valor a sacar:");
-            String withdrawAmount = scanner.nextLine(); 
+            double withdrawAmount = scanner.nextDouble(); 
 
-            System.out.println("Nome do usuário: " + userName); 
-            System.out.println("Valor depositado: " + depositAmount + "R$"); 
-            System.out.println("Valor sacado: " + withdrawAmount + "R$");
+            if (withdrawAmount > bankAccount) 
+            {
+                System.out.println("O valor solicitado não está disponiível!");  
+            } else
+            {
+                System.out.println("Nome do usuário: " + userName); 
+                System.out.println("Valor depositado: " + depositAmount + "R$"); 
+                System.out.println("Valor sacado: " + withdrawAmount + "R$");
+                System.out.println("Saldo: " + (bankAccount +depositAmount - withdrawAmount));
+            }
+        } catch (InputMismatchException e){
+            System.err.println("Por favor, digite apenas números nos valores a sacar e depositar!");
         }
     }
 }
